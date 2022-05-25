905aafb113f810adc21c8b31de1aac75b8cc3f9c
João Vitor C. de Campos <56615126+Johnman67112@users.noreply.github.com>
2022-05-25 12:00:55 -0300

    Getting every data from database (#6)

---
+++ controllers/price.go
@@ -0,0 +1,91 @@
+package controllers
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/Keanus-In-Reevesverse/website-structure/database"
+	"github.com/Keanus-In-Reevesverse/website-structure/models"
+	"github.com/gin-gonic/gin"
+)
+
+//Get arrays
+func GetPrices(c *gin.Context) {
+	var prices []models.GamePrice
+	database.DB.Table("GAME_PRICES").Find(&prices)
+	c.JSON(http.StatusOK, &prices)
+}
+
+func GetPriceAlerts(c *gin.Context) {
+	var alerts []models.PriceAlert
+	database.DB.Table("PRICE_ALERT").Find(&alerts)
+	c.JSON(http.StatusOK, &alerts)
+}
+
+//Get by id
+func GetPricesByGameId(c *gin.Context) {
+	var prices []models.GamePrice
+	id := c.Params.ByName("game_id")
+
+	database.DB.Table("GAME_PRICES").Find(&prices).Where("game_id = ?", id)
+
+	for _, price := range prices {
+		if price.GameId == 0 {
+			c.JSON(http.StatusNotFound, gin.H{
+				"Not Found": "Game not found"})
+			return
+		}
+		if price.StoreId == 0 {
+			c.JSON(http.StatusNotFound, gin.H{
+				"Not Found": "Store not found"})
+			return
+		}
+	}
+
+	c.JSON(http.StatusOK, prices)
+}
+
+func GetPricesByStoreId(c *gin.Context) {
+	var prices []models.GamePrice
+	id := c.Params.ByName("store_id")
+
+	database.DB.Table("GAME_PRICES").Find(&prices).Where("store_id = ?", id)
+
+	for i, price := range prices {
+		if fmt.Sprintf("%c", price.StoreId) != id {
+			c.JSON(http.StatusNotFound, gin.H{
+				"Not Found": "Wrong Store"})
+			return
+		}
+		if price.GameId == 0 {
+			prices = append(prices[:i], prices[i+1:]...)
+		}
+	}
+
+	c.JSON(http.StatusOK, prices)
+}
+
+func GetAlertsByIds(c *gin.Context) {
+	var alerts []models.PriceAlert
+	user_id := c.Params.ByName("user_id")
+	game_id := c.Params.ByName("game_id")
+
+	if game_id != "" {
+		database.DB.Table("ALERT_PRICES").Find(&alerts).Where("user_id = ?", user_id)
+	} else {
+		database.DB.Table("ALERT_PRICES").Find(&alerts).Where("user_id = ? AND game_id = ?", user_id, game_id)
+	}
+
+	for i, alert := range alerts {
+		if alert.UserId == 0 {
+			c.JSON(http.StatusNotFound, gin.H{
+				"Not Found": "User Not Found"})
+			return
+		}
+		if alert.GameId == 0 {
+			alerts = append(alerts[:i], alerts[i+1:]...)
+		}
+	}
+
+	c.JSON(http.StatusOK, alerts)
+}
